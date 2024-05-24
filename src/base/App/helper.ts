d4a922f41c104a443d3c0924b350e0c583bb7e0a
DyNamite-TNT-1 <lemauanhduc@hanbiro.com>
2024-05-24 13:10:36 +0700

    add

--- src/base/App/helper.ts
+++ src/base/App/helper.ts
@@ -1,39 +1,35 @@
+import { RangeSelection } from "lexical";
+import {
+    $isAtNodeEnd,
+} from "@lexical/selection";
 
-// Text node formatting // https://github.com/facebook/lexical/blob/main/packages/lexical/src/LexicalConstants.ts
-export const IS_BOLD = 1; //1
-export const IS_ITALIC = 1 << 1; //2
-export const IS_STRIKETHROUGH = 1 << 2; //4
-export const IS_UNDERLINE = 1 << 3; //8
-export const IS_CODE = 1 << 4; //16
-export const IS_SUBSCRIPT = 1 << 5; //32
-export const IS_SUPERSCRIPT = 1 << 6; //64
-export const IS_HIGHLIGHT = 1 << 7; //128
+export const blockTypeToBlockName = {
+    bullet: 'Bulleted List',
+    check: 'Check List',
+    code: 'Code Block',
+    h1: 'Heading 1',
+    h2: 'Heading 2',
+    h3: 'Heading 3',
+    h4: 'Heading 4',
+    h5: 'Heading 5',
+    h6: 'Heading 6',
+    number: 'Numbered List',
+    paragraph: 'Normal',
+    quote: 'Quote',
+};
 
-export function analyzeSumOfConstants(number: number) {
-    const constants = [
-        { name: "IS_BOLD", value: 1 },
-        { name: "IS_ITALIC", value: 2 },
-        { name: "IS_STRIKETHROUGH", value: 4 },
-        { name: "IS_UNDERLINE", value: 8 },
-        { name: "IS_CODE", value: 16 },
-        { name: "IS_SUBSCRIPT", value: 32 },
-        { name: "IS_SUPERSCRIPT", value: 64 },
-        { name: "IS_HIGHLIGHT", value: 128 },
-    ];
-
-    const binary = number.toString(2);
-
-    const selectedConstants = constants.filter(
-        (constant) => (number & constant.value) === constant.value
-    );
-
-    const sumFormula = selectedConstants
-        .map((constant) => constant.name)
-        .join(" + ");
-
-    console.log(
-        `${number} = ${binary} = ${sumFormula}. Following that sum formula.`
-    );
-
-    return selectedConstants;
+export function getSelectedNode(selection: RangeSelection) {
+    const anchor = selection.anchor;
+    const focus = selection.focus;
+    const anchorNode = selection.anchor.getNode();
+    const focusNode = selection.focus.getNode();
+    if (anchorNode === focusNode) {
+        return anchorNode;
+    }
+    const isBackward = selection.isBackward();
+    if (isBackward) {
+        return $isAtNodeEnd(focus) ? anchorNode : focusNode;
+    } else {
+        return $isAtNodeEnd(anchor) ? focusNode : anchorNode;
+    }
 }
--- src/base/App/index.tsx
+++ src/base/App/index.tsx
@@ -1,13 +1,18 @@
+// third-party
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 
-import ExampleTheme from "./ExampleTheme";
-import "@base/assets/css/app.css";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import {
+  $getNearestNodeOfType,
+  $findMatchingParent,
+  mergeRegister,
+} from "@lexical/utils";
 import {
   FORMAT_TEXT_COMMAND,
   UNDO_COMMAND,
@@ -15,21 +20,53 @@ import {
   FORMAT_ELEMENT_COMMAND,
   INDENT_CONTENT_COMMAND,
   OUTDENT_CONTENT_COMMAND,
-  EditorState,
-  $getRoot,
   $getSelection,
   $isRangeSelection,
+  $isRootOrShadowRoot,
+  $createParagraphNode,
+  SELECTION_CHANGE_COMMAND,
+  CAN_UNDO_COMMAND,
+  CAN_REDO_COMMAND,
+  COMMAND_PRIORITY_CRITICAL,
+  COMMAND_PRIORITY_NORMAL,
+  ElementFormatType,
+  $isElementNode,
 } from "lexical";
-import { useEffect } from "react";
-import TreeViewPlugin from "@base/plugins/TreeViewPlugin";
-import ToolbarPlugin from "@base/plugins/ToolbarPlugin";
+import {
+  $isParentElementRTL,
+  $setBlocksType,
+} from "@lexical/selection";
+import {
+  $createHeadingNode,
+  $createQuoteNode,
+  $isHeadingNode,
+  HeadingTagType,
+} from "@lexical/rich-text";
+import {
+  INSERT_ORDERED_LIST_COMMAND,
+  INSERT_UNORDERED_LIST_COMMAND,
+  $isListNode,
+  ListNode,
+  ListItemNode,
+} from "@lexical/list";
+import {
+  $createCodeNode,
+} from "@lexical/code";
+import { $isLinkNode } from "@lexical/link";
+import { useCallback, useEffect, useState } from "react";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
-import { ListItemNode, ListNode } from "@lexical/list";
 import { CodeHighlightNode, CodeNode } from "@lexical/code";
 import { AutoLinkNode, LinkNode } from "@lexical/link";
-import { analyzeSumOfConstants } from "./helper";
-
+// base
+import ExampleTheme from "./ExampleTheme";
+import "@base/assets/css/app.css";
+import AutoLinkPlugin from "@base/plugins/AutoLinkPlugin";
+import TreeViewPlugin from "@base/plugins/TreeViewPlugin";
+import ToolbarPlugin from "@base/plugins/ToolbarPlugin";
+import CodeHighlightPlugin from "@base/plugins/CodeHighlightPlugin";
+import ListMaxIndentLevelPlugin from "@base/plugins/ListMaxIndentLevelPlugin";
+import { blockTypeToBlockName, getSelectedNode } from "./helper";
 const editorConfig = {
   namespace: "React.js Demo",
   nodes: [
@@ -53,17 +90,6 @@ const editorConfig = {
   theme: ExampleTheme,
 };
 
-
-function onChange(editorState: EditorState) {
-  editorState.read(() => {
-    const selection = $getSelection();
-    console.log(selection);
-    if ($isRangeSelection(selection)) {
-      if (selection.format !== 0) analyzeSumOfConstants(selection.format);
-    }
-  });
-}
-
 declare global {
   interface Window {
     undo: () => void;
@@ -79,6 +105,13 @@ declare global {
     alignCenter: () => void;
     alignRight: () => void;
     alignJustify: () => void;
+    formatParagraph: () => void;
+    formatHeading: (headingSize: HeadingTagType) => void;
+    formatNumberedList: () => void;
+    formatBulletList: () => void;
+    formatQuote: () => void;
+    formatCodeBlock: () => void;
+    onChangeStatusButton: ({}) => void;
   }
 }
 
@@ -92,6 +125,176 @@ function AutoFocusPlugin() {
 
 function MyFunctionPlugin() {
   const [editor] = useLexicalComposerContext();
+  const [activeEditor, setActiveEditor] = useState(editor);
+  const [canUndo, setCanUndo] = useState(false);
+  const [canRedo, setCanRedo] = useState(false);
+  const [blockType, setBlockType] = useState("paragraph");
+  const [selectedElementKey, setSelectedElementKey] = useState<string | null>(
+    null
+  );
+
+  const [codeLanguage, setCodeLanguage] = useState("");
+  const [isRTL, setIsRTL] = useState(false);
+  const [elementFormat, setElementFormat] = useState<ElementFormatType>("left");
+  const [isLink, setIsLink] = useState(false);
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isUnderline, setIsUnderline] = useState(false);
+  const [isStrikethrough, setIsStrikethrough] = useState(false);
+  const [isSubscript, setIsSubscript] = useState(false);
+  const [isSuperscript, setIsSuperscript] = useState(false);
+  const [isCode, setIsCode] = useState(false);
+
+  const $updateToolbar = useCallback(() => {
+    const selection = $getSelection();
+    if ($isRangeSelection(selection)) {
+      const anchorNode = selection.anchor.getNode();
+      let element =
+        anchorNode.getKey() === "root"
+          ? anchorNode
+          : $findMatchingParent(anchorNode, (e) => {
+              const parent = e.getParent();
+              return parent !== null && $isRootOrShadowRoot(parent);
+            });
+
+      if (element === null) {
+        element = anchorNode.getTopLevelElementOrThrow();
+      }
+
+      const elementKey = element.getKey();
+      const elementDOM = activeEditor.getElementByKey(elementKey);
+      // Update text format
+      setIsBold(selection.hasFormat("bold"));
+      setIsItalic(selection.hasFormat("italic"));
+      setIsUnderline(selection.hasFormat("underline"));
+      setIsStrikethrough(selection.hasFormat("strikethrough"));
+      setIsSubscript(selection.hasFormat("subscript"));
+      setIsSuperscript(selection.hasFormat("superscript"));
+      setIsCode(selection.hasFormat("code"));
+      setIsRTL($isParentElementRTL(selection));
+
+      // Update links
+      const node = getSelectedNode(selection);
+      const parent = node.getParent();
+      if ($isLinkNode(parent) || $isLinkNode(node)) {
+        setIsLink(true);
+      } else {
+        setIsLink(false);
+      }
+
+      if (elementDOM !== null) {
+        setSelectedElementKey(elementKey);
+        if ($isListNode(element)) {
+          const parentList = $getNearestNodeOfType<ListNode>(
+            anchorNode,
+            ListNode
+          );
+          const type = parentList
+            ? parentList.getListType()
+            : element.getListType();
+          setBlockType(type);
+        } else {
+          const type = $isHeadingNode(element)
+            ? element.getTag()
+            : element.getType();
+          if (type in blockTypeToBlockName) {
+            setBlockType(type as keyof typeof blockTypeToBlockName);
+          }
+          // if ($isCodeNode(element)) {
+          //   const language =
+          //     element.getLanguage() as keyof typeof CODE_LANGUAGE_MAP;
+          //   setCodeLanguage(
+          //     language ? CODE_LANGUAGE_MAP[language] || language : ""
+          //   );
+          //   return;
+          // }
+        }
+      }
+      let matchingParent;
+      if ($isLinkNode(parent)) {
+        // If node is a link, we need to fetch the parent paragraph node to set format
+        matchingParent = $findMatchingParent(
+          node,
+          (parentNode) => $isElementNode(parentNode) && !parentNode.isInline()
+        );
+      }
+      setElementFormat(
+        $isElementNode(matchingParent)
+          ? matchingParent.getFormatType()
+          : $isElementNode(node)
+            ? node.getFormatType()
+            : parent?.getFormatType() || "left"
+      );
+    }
+  }, [activeEditor]);
+
+  useEffect(() => {
+    return editor.registerCommand(
+      SELECTION_CHANGE_COMMAND,
+      (_payload, newEditor) => {
+        $updateToolbar();
+        setActiveEditor(newEditor);
+        return false;
+      },
+      COMMAND_PRIORITY_CRITICAL
+    );
+  }, [editor, $updateToolbar]);
+
+  useEffect(() => {
+    return mergeRegister(
+      activeEditor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          $updateToolbar();
+        });
+      }),
+      activeEditor.registerCommand<boolean>(
+        CAN_UNDO_COMMAND,
+        (payload) => {
+          setCanUndo(payload);
+          return false;
+        },
+        COMMAND_PRIORITY_CRITICAL
+      ),
+      activeEditor.registerCommand<boolean>(
+        CAN_REDO_COMMAND,
+        (payload) => {
+          setCanRedo(payload);
+          return false;
+        },
+        COMMAND_PRIORITY_CRITICAL
+      )
+    );
+  }, [$updateToolbar, activeEditor, editor]);
+
+  useEffect(() => {
+    window.onChangeStatusButton(
+      JSON.stringify({
+        isRTL,
+        isLink,
+        isBold,
+        isItalic,
+        isUnderline,
+        isStrikethrough,
+        isCode,
+        canUndo,
+        canRedo,
+        blockType,
+        elementFormat,
+      })
+    );
+  }, [
+    isRTL,
+    isLink,
+    isBold,
+    isItalic,
+    isUnderline,
+    isStrikethrough,
+    isCode,
+    canUndo,
+    canRedo,
+    blockType,
+    elementFormat,
+  ]);
 
   window.undo = function () {
     editor.dispatchCommand(UNDO_COMMAND, undefined);
@@ -145,6 +348,80 @@ function MyFunctionPlugin() {
     editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "justify");
   };
 
+  window.formatParagraph = function () {
+    editor.update(() => {
+      const selection = $getSelection();
+
+      if ($isRangeSelection(selection)) {
+        $setBlocksType(selection, () => $createParagraphNode());
+      }
+    });
+  };
+
+  window.formatHeading = (headingSize: HeadingTagType) => {
+    if (blockType !== headingSize) {
+      editor.update(() => {
+        const selection = $getSelection();
+        $setBlocksType(selection, () => $createHeadingNode(headingSize));
+      });
+    }
+  };
+
+  window.formatBulletList = () => {
+    console.log(blockType);
+    if (blockType !== "bullet") {
+      editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined);
+    } else {
+      window.formatParagraph();
+    }
+  };
+
+  window.formatNumberedList = () => {
+    if (blockType !== "number") {
+      editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined);
+    } else {
+      window.formatParagraph();
+    }
+  };
+
+  window.formatQuote = () => {
+    if (blockType !== "quote") {
+      editor.update(() => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          $setBlocksType(selection, () => $createQuoteNode());
+        }
+      });
+    }
+  };
+
+  window.formatCodeBlock = () => {
+    if (blockType !== "code") {
+      editor.update(() => {
+        let selection = $getSelection();
+
+        if (selection !== null) {
+          if (selection.isCollapsed()) {
+            $setBlocksType(selection, () => $createCodeNode());
+          } else {
+            const textContent = selection.getTextContent();
+            const codeNode = $createCodeNode();
+            selection.insertNodes([codeNode]);
+            selection = $getSelection();
+            if ($isRangeSelection(selection)) {
+              selection.insertRawText(textContent);
+            }
+          }
+        }
+      });
+    }
+  };
+
+  window.onChangeStatusButton = function (map: any) {
+    console.log(JSON.parse(map));
+  };
+
   return null;
 }
 
@@ -154,6 +431,7 @@ export default function App() {
       <LexicalComposer initialConfig={editorConfig}>
         <div className="editor-container">
           <MyFunctionPlugin />
+          <AutoLinkPlugin />
           <ToolbarPlugin />
           <div className="editor-inner">
             <RichTextPlugin
@@ -165,10 +443,15 @@ export default function App() {
               }
               ErrorBoundary={LexicalErrorBoundary}
             />
-            <OnChangePlugin onChange={onChange} />
             {/* History Plugin is necessary if want to have undo, redo*/}
             <HistoryPlugin />
-            <TreeViewPlugin />
+            <LinkPlugin />
+            {/* ListPlugin is necessary for numbered list, bullet list */}
+            <ListPlugin />
+            {/* CodeHighlightPlugin is necessary for code block */}
+            <CodeHighlightPlugin />
+            <ListMaxIndentLevelPlugin maxDepth={7} />
+            {/* <TreeViewPlugin /> This plugin to use when debugging*/}
             <AutoFocusPlugin />
           </div>
         </div>
